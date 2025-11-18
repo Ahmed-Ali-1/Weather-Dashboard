dfdb903d3f1f9c688235e8a59413f58e99369f13
Ahmed-Ali-1 <ahmedali034012449@gmail.com>
2025-11-18 13:15:11 +0500

    Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -149,3 +149,68 @@ function renderRecentSearches() {
         recentList.appendChild(li);
     });
 }
+
+
+
+
+
+
+// const apiKey = "0e59dae15ac0a8aa871a6987a34e2380"
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+// const cityInput = document.getElementById('cityInput');
+// const searchBtn = document.getElementById('searchBtn');
+// const weatherImage = document.getElementById('weatherImage')
+// const loadingText = document.getElementById('loading');
+// const errorText = document.getElementById('error');
+// const recentList = document.getElementById('recentList');
+
+// async function checkWeather(city) {
+//     document.querySelector(`.weather-card`).style.display = "none"
+//     loadingText.style.display = "block";
+//     errorText.style.display = "none";
+
+//     try {
+//         const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+//         if (!response.ok) {
+//             throw new Error("City not found");
+//         }
+
+//         const data = await response.json();
+//         loadingText.style.display = "none";
+
+//         document.getElementById('cityName').innerHTML = data.name;
+//         document.getElementById('weatherType').innerHTML = data.weather[0].main;
+//         document.getElementById('temp').innerHTML = Math.round(data.main.temp) + "°C";
+//         document.getElementById('humidity').innerHTML = data.main.humidity + "%";
+//         document.getElementById('wind').innerHTML = data.wind.speed + "km/h ";
+
+//         if (data.weather[0].main == "Clouds") {
+//             weatherImage.src = ".//assets/images/cloudy.png"
+
+//         } else if (data.weather[0].main == "Clear") {
+//             weatherImage.src = ".//assets/images/sunny.png"
+
+//         } else if (data.weather[0].main == "Rain") {
+//             weatherImage.src = ".//assets/images/heavy-rain.png"
+
+//         } else if (data.weather[0].main == "Drizzle") {
+//             weatherImage.src = ".//assets/images/drizzle.png"
+
+//         } else if (data.weather[0].main == "Mist") {
+//             weatherImage.src = ".//assets/images/mist.png"
+//         }
+//         document.querySelector(`.weather-card`).style.display = "block"
+
+//     } catch (error) {
+//         loadingText.style.display = "none";
+//         errorText.style.display = "block";
+//     }
+
+// }
+
+// searchBtn.addEventListener('click', () => {
+//     checkWeather(cityInput.value.trim());
+//     cityInput.value = ""
+
+// })
